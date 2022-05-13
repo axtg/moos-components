49ab72f58608101225ac752673e3895cc31125ca
Xander Groesbeek <xander@xadi.nl>
2022-05-13 15:38:55 +0200

    Correct non-existant image

--- src/components/MCard/MCard.vue
+++ src/components/MCard/MCard.vue
@@ -2,7 +2,7 @@
     <!-- Vertical -->
     <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-            <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            <img class="rounded-t-lg" src="https://picsum.photos/200" alt="" />
         </a>
         <div class="p-5">
             <a href="#">
@@ -18,7 +18,7 @@
     
     <!-- Horizontal -->
     <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
+        <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://picsum.photos/200" alt="">
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
